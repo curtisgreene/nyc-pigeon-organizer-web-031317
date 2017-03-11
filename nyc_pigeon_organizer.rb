c42551b9511d8495a4048b611988f36885128bbe
Curtis Greene <curtis.j.w.greene@gmail.com>
2017-03-11 23:15:59 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_hash = {}
+  data.each do |attribute, options|
+    options.each do |option, names|
+      names.each do |name|
+      new_hash[name] ||= {}
+      new_hash[name][attribute] ||= []
+      new_hash[name][attribute] << option.to_s
+      end
+    end
+  end
+  new_hash
 end
